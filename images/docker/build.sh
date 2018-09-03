2a081a152e17c42b45d675838c4809157ccf049b
Alexander Mustafin <mustafin.aleksandr@gmail.com>
2018-09-03 08:45:03 +0500

    Migrated to Debian Stretch

--- images/docker/build.sh
+++ images/docker/build.sh
@@ -11,31 +11,38 @@ export RAKE_ENV=production
 export BUNDLE_GEMFILE=/home/yeti-web/Gemfile
 export GEM_PATH=/home/yeti-web/vendor/bundler
 
+log::m-info "Installing essentials ..."
+apt-get update -qq && apt-get install -yqq wget ca-certificates
+
 log::m-info "Adding Yeti repo to the /etc/apt"
-echo "deb http://pkg.yeti-switch.org/debian/jessie stable main ext" >> /etc/apt/sources.list
+echo "deb http://pkg.yeti-switch.org/debian/stretch 1.6 main" >> /etc/apt/sources.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" >> /etc/apt/sources.list
 
 log::m-info "Let's add the repo signature"
-apt-key adv --keyserver keys.gnupg.net --recv-key 9CEBFFC569A832B6
+#apt-key adv --keyserver keys.gnupg.net --recv-key 9CEBFFC569A832B6
+wget http://pkg.yeti-switch.org/key.gpg -O - | apt-key add -
+wget https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
 
 log::m-info "Installing essentials ..."
 apt-get update -qq
 apt-get install -yqq \
-	postgresql-client-9.4 \
-	postgresql-contrib-9.4 \
-	postgresql-9.4-prefix \
-    postgresql-9.4-pgq3 \
-    postgresql-9.4-yeti \
-	build-essential \
-	libpq-dev \
-	patch \
+    postgresql-10 \
+    postgresql-client-10 \
+    postgresql-contrib-10 \
+    postgresql-10-prefix \
+    postgresql-10-pgq3 \
+    postgresql-10-yeti \
+    postgresql-10-pgq-ext \
+    build-essential \
+    libpq-dev \
+    patch \
     zlib1g-dev \
     liblzma-dev \
     ruby2.3-dev \
-	ca-certificates \
-	ruby2.3 \
-	iputils-ping \
-        curl \
-	git
+    ruby2.3 \
+    iputils-ping \
+    curl \
+    git
 
 log::m-info "Clone $BUILD branch of $APP Release ..."
 git clone -b $BUILD https://github.com/yeti-switch/yeti-web.git /home/yeti-web
@@ -56,6 +63,17 @@ export RAKE_ENV=production
 export BUNDLE_GEMFILE=/home/yeti-web/Gemfile
 export GEM_PATH=/home/yeti-web/vendor/bundler
 gem install bundler
+
+
+log::m-info "Writing $APP database.yml ..."
+cat > /home/yeti-web/config/yeti_web.yml <<EOF
+site_title: "Yeti Admin"
+site_title_image: "yeti.png"
+api:
+  token_lifetime: 600 # jwt token lifetime in seconds, empty string means permanent tokens
+cdr_export:
+  dir_path: "/tmp"
+EOF
 
 log::m-info "Writing $APP database.yml ..."
 cat > /home/yeti-web/config/database.yml <<EOF
